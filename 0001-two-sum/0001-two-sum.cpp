3b3a886467c943920a31ec3be7a09a4ec25405e4
Muhammad Younus Peerzada <pakistan14612@gmail.com>
2026-07-30 07:05:19 +0500

    Time: 3 ms (66.38%), Space: 14.9 MB (21.78%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen;
+
         for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
+            int complement = target - nums[i];
+
+            if (seen.count(complement)) {
+                return {seen[complement], i};
             }
+
+            seen[nums[i]] = i;
         }
+
         return {};
-    }
+    };
 };
